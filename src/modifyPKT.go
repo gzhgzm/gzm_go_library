28b2d0045d94a4688fb87b72b6428bdeb4484124
gzhgzm <55232442+gzhgzm@users.noreply.github.com>
2023-10-09 23:48:09 +0800

    学习使用gopacket

--- src/modifyPKT.go
+++ src/modifyPKT.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"os"
 	"common"
 	"fmt"
+	"os"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
@@ -37,8 +38,24 @@ func test1() {
 	pktSource := gopacket.NewPacketSource(pktHandle, pktHandle.LinkType())
 
 	for packet := range pktSource.Packets() {
-		fmt.Println(packet)
+		//fmt.Println(packet)
+		showPacket(packet)
 	}
 }
+
+func showPacket(pkt gopacket.Packet) {
+	L1 := pkt.Layer(layers.LayerTypeEthernet)
+	if L1 != nil {
+		ethernetL1, _ := L1.(*layers.Ethernet)
+		fmt.Printf("%v \n\n", ethernetL1)
+	}
+
+	L2 := pkt.Layer(layers.LayerTypeIPv4)
+	if L2 != nil {
+		ipL2, _ := L2.(*layers.IPv4)
+		fmt.Printf("%v \n\n", ipL2)
+	}
+}
+
 
 
